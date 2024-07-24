8d1d9b5f47fbba408f49fc5d521d6b4faef67014
Rajik1984 <146588687+Rajik1984@users.noreply.github.com>
2024-07-25 00:38:18 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,28 @@ provider "google" {
   region      = var.region
 }
 
-variable "project_id" {}
-variable "region" {}
-variable "gcp_sa_key" {}
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+}
+
+variable "gcp_sa_key" {
+  description = "The base64 encoded GCP service account key"
+  type        = string
+}
 
 resource "google_storage_bucket" "default" {
-  name     = "rs-gcp-flask"
+  name     = "your-gcp-bucket-name"
   location = var.region
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.default.name
 }
 
 
