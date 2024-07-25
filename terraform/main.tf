c0f1ad18abd96268a0de795646e96d2aed89316e
Rajik1984 <146588687+Rajik1984@users.noreply.github.com>
2024-07-25 11:35:15 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,18 @@
+# terraform/main.tf
+
 provider "google" {
-  credentials = filebase64decode(var.gcp_sa_key)
-  project     = var.project_id
-  region      = var.region
+  project = var.project_id
+  region  = var.region
 }
 
-variable "project_id" {
-  description = "The GCP project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "The GCP region"
-  type        = string
-}
-
-variable "gcp_sa_key" {
-  description = "The base64 encoded GCP service account key"
-  type        = string
-}
-
-resource "google_storage_bucket" "default" {
-  name     = "your-gcp-bucket-name"
+resource "google_artifact_registry_repository" "repository" {
+  name     = var.repo_name
+  format   = "DOCKER"
   location = var.region
 }
 
-output "bucket_name" {
-  value = google_storage_bucket.default.name
+output "artifact_registry_repository" {
+  value = google_artifact_registry_repository.repository.name
 }
-
 
 
